5b2a3610f8a12595a8b3475ac14fa977ef72578a
Ruslan <ruslan-karamurzanov@mail.ru>
2021-02-20 17:27:52 +0700

    new commit get

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/public'))) 
 
-const port = prosecc.env.PORT || 8080 
+const port = process.env.PORT || 8080 
 app.listen(port)
 
 console.log('Linstenning on port: ' + port)
